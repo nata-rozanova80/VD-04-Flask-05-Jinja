df5bb3a18e6520dd23685135c071f849ea2cc14d
nata-rozanova80 <nata.rozanova80@gmail.com>
2024-10-11 23:36:08 +0300

    Blog page done commit

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    day_of_week = ['sun', 'mon', 'tue', 'wed', 'thr', 'fri', 'sat']
+    days_in_month = 31
+    return render_template('index.html', day_of_week=day_of_week, days_in_month=days_in_month)
 
-@app.route('/blog')
+@app.route('/blog')  # Исправлено: route должен быть одинарным слэшем
 def blog():
     return render_template('blog.html')
 
-@app.route('/contacts')
+@app.route('/contacts')  # Исправлено: route должен быть одинарным слэшем
 def contacts():
     return render_template('contacts.html')
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Исправлено: __name__ и '__main__' вместо name и main
     app.run(debug=True)
-
