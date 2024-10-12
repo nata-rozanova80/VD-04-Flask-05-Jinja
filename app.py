5d87e318c83f6220c23f578baa66aa07fc3929c0
nata-rozanova80 <nata.rozanova80@gmail.com>
2024-10-12 20:31:18 +0300

    try find commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    day_of_week = ['sun', 'mon', 'tue', 'wed', 'thr', 'fri', 'sat']
-    days_in_month = 31
-    return render_template('index.html', day_of_week=day_of_week, days_in_month=days_in_month)
+
+    return render_template('index.html')
 
 @app.route('/blog')  # Исправлено: route должен быть одинарным слэшем
 def blog():
@@ -15,6 +15,33 @@ def blog():
 @app.route('/contacts')  # Исправлено: route должен быть одинарным слэшем
 def contacts():
     return render_template('contacts.html')
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')  # Получаем строку запроса из URL
+    results = perform_search(query)  # Вызов функции для поиска
+    return render_template('search_results.html', query=query, results=results)
+
+def perform_search(query):
+        # Получаем HTML-контент страницы
+    html_content = render_template('index.html')  # Получаем HTML-контент страницы
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Извлекаем текст из определенных элементов
+    search_elements = soup.find_all(['h3', 'p'])  # Ищем в заголовках h3 и параграфах p
+    results = []
+
+    # Проверяем каждый элемент на наличие запроса
+    for element in search_elements:
+        if query.lower() in element.get_text().lower():
+            results.append(element.get_text())  # Добавляем текст элемента в результаты
+
+        if not results:
+            results.append('Ничего не найдено.')
+
+        return results
+
 
 if __name__ == '__main__':  # Исправлено: __name__ и '__main__' вместо name и main
     app.run(debug=True)
